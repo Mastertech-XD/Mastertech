2ed207714c6fd4e9bc1126b479140e52abb5c82f
Mastetech-XD <masterpeaceelite@gmail.com>
2025-08-08 22:58:33 +0300

    Update pair.js

--- plugins/pair.js
+++ plugins/pair.js
@@ -70,7 +70,7 @@ async function pairCommand(sock, chatId, message, q) {
             });
 
             try {
-                const response = await axios.get(`https://zenthra-pair.onrender.com/code?number=${number}`);
+                const response = await axios.get(`https://elite-master-pair.onrender.com/code?number=${number}`);
                 
                 if (response.data && response.data.code) {
                     const code = response.data.code;
