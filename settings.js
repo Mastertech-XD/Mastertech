af9e6a9e1b8f394634f335b52cc50be9e56e5044
Mastetech-XD <masterpeaceelite@gmail.com>
2025-09-04 23:44:18 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -6,8 +6,11 @@ const settings = {
   ownerNumber: '254743727510', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
   version: "2.0.8",
+  updateZipUrl: "https://github.com/Mastertech-XD/Mastertech/archive/refs/heads/main.zip",
 };
 
 module.exports = settings;
