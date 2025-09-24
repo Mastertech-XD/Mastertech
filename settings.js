ba22985fab5d00c6398b59875bf98d092b571ac4
Mastertech-XD <masterpeaceelite@gmail.com>
2025-09-24 12:08:55 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,16 @@
-
-module.exports = {
-  SESSION_ID: "",  // add your Session Id here
-  
-  OWNER_NUMBER: "254743727510", // put your phone number here
-  
-  PREFIX: ".", // prefix (e.g., ., /, !, *)
-  
-  TIMEZONE: "Africa/Nairobi" //put your country timeZone....leave blank if u don't know.
+const settings = {
+  packname: 'Mastertech-XD',
+  author: '‎',
+  botName: "Mastertech-XD",
+  botOwner: 'Masterpeace Elite', // Your name
+  ownerNumber: '254743727510', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "2.0.8",
+  updateZipUrl: "https://github.com/Mastertech-XD/Mastertech/archive/refs/heads/main.zip",
 };
+
+module.exports = settings;
